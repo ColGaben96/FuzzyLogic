82a9cb29b49259e988b49a4feea558f93e5bd2a7
Gabriel Blanco La Rotta <geblarot96@gmail.com>
2019-05-21 17:32:02 -0500

    Update Lel.java

--- src/com/FuzzyLogic/Lel.java
+++ src/com/FuzzyLogic/Lel.java
@@ -1,5 +1,13 @@
 package com.FuzzyLogic;
 
-public class Lel {
+import net.sourceforge.jFuzzyLogic.*;
 
+public class Lel 
+{
+	public static void main(String[] args) 
+	{
+		//A LA VERGA
+		int i = 1;
+		System.out.println(i);
+	}
 }
